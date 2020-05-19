802db4a7235bf60f8b55e57c8b127214d9cb9a78
grafijoss <grafijoss@hotmail.com>
2020-05-19 08:38:40 -0500

    mejorra conexion

--- ControlClientes/src/main/java/datos/Conexion.java
+++ ControlClientes/src/main/java/datos/Conexion.java
@@ -8,14 +8,20 @@ public class Conexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/contol_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "admin";
+
+		private static BasicDataSource dataSource;
     
     public static DataSource getDataSource(){
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(JDBC_URL);
-        ds.setUsername(JDBC_USER);
-        ds.setPassword(JDBC_PASSWORD);
-        ds.setInitialSize(50);
-        return ds;
+
+			if (dataSource == null){ // solo creamos una nueva conexion a la base de  datos en el caso de que no exista
+        dataSource = new BasicDataSource();
+        dataSource.setUrl(JDBC_URL);
+        dataSource.setUsername(JDBC_USER);
+        dataSource.setPassword(JDBC_PASSWORD);
+        dataSource.setInitialSize(50);
+			}
+
+        return dataSource;
     }
     
     public static Connection getConnection() throws SQLException{
